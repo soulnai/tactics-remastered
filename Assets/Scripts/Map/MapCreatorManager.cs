113610619edff2eadf3875c817d557da95730fb0
soulnai <soulnai@gmail.com>
2015-06-03 17:45:32 +0300

    load map in editor with stuff

--- Assets/Scripts/Map/MapCreatorManager.cs
+++ Assets/Scripts/Map/MapCreatorManager.cs
@@ -60,26 +60,59 @@ public class MapCreatorManager : MonoBehaviour {
 		MapXmlContainer container = MapSaveLoad.Load("map.xml");
 
 		mapSize = container.size;
-		
-		//initially remove all children
-		for(int i = 0; i < mapTransform.childCount; i++) {
-			Destroy (mapTransform.GetChild(i).gameObject);
-		}
 
-		map = new List<List<Tile>>();
-		for (int i = 0; i < mapSize; i++) {
-			List <Tile> row = new List<Tile>();
-			for (int j = 0; j < mapSize; j++) {
-				float tileHeight = container.tiles.Where(x => x.locX == i && x.locY == j).First().height;
-                Tile tile = ((GameObject)Instantiate(GM.Prefabs.BASE_TILE_PREFAB, new Vector3(i - Mathf.Floor(mapSize / 2), tileHeight, -j + Mathf.Floor(mapSize / 2)), Quaternion.Euler(new Vector3()))).GetComponent<Tile>();
-				tile.transform.parent = mapTransform;
-				tile.gridPosition = new Vector2(i, j);
-				tile.setType((TileType)container.tiles.Where(x => x.locX == i && x.locY == j).First().id);
-				tile.height = tileHeight;
-				row.Add (tile);
-			}
-			map.Add(row);
-		}
+        for (int i = 0; i < mapTransform.childCount; i++)
+        {
+            Destroy(mapTransform.GetChild(i).gameObject);
+        }
+
+        map = new List<List<Tile>>();
+        for (int i = 0; i < mapSize; i++)
+        {
+            List<Tile> row = new List<Tile>();
+            for (int j = 0; j < mapSize; j++)
+            {
+                float tileHeight = container.tiles.Where(x => x.locX == i && x.locY == j).First().height;
+                Tile tile = ((GameObject)Instantiate(GM.Prefabs.BASE_TILE_PREFAB, new Vector3(i - Mathf.Floor(GM.Scenario.mapSize / 2), tileHeight, -j + Mathf.Floor(GM.Scenario.mapSize / 2)), Quaternion.Euler(new Vector3()))).GetComponent<Tile>();
+
+                tile.transform.parent = mapTransform;
+                tile.gridPosition = new Vector2(i, j);
+                tile.setType((EnumSpace.TileType)container.tiles.Where(x => x.locX == i && x.locY == j).First().id);
+                tile.height = tileHeight;
+                row.Add(tile);
+                GM.BattleData.allTiles.Add(tile);
+            }
+            map.Add(row);
+        }
+        GM.Scenario.map = map;
+        for (int i = 0; i < mapSize; i++)
+        {
+            for (int j = 0; j < mapSize; j++)
+            {
+                //map[i][j].generateNeighbors();
+            }
+        }
+
+        int treesCount = container.trees.Count;
+        for (int i = 0; i < treesCount; i++)
+        {
+            string stuffType = container.trees.ElementAt(i).prefabName;
+            Tile tileTospawn = GM.Scenario.map[container.trees.ElementAt(i).locX][container.trees.ElementAt(i).locY];
+            Instantiate(GM.Prefabs.Prefabs[stuffType], tileTospawn.transform.position, Quaternion.Euler(-90, 90, 0));
+            tileTospawn.impassible = true;
+            GM.BattleData.blockedTiles.Add(tileTospawn);
+        }
+
+        int cratesCount = container.crates.Count;
+
+        for (int i = 0; i < cratesCount; i++)
+        {
+            string stuffType = container.crates.ElementAt(i).prefabName;
+            Tile tileTospawn = GM.Scenario.map[container.crates.ElementAt(i).locX][container.crates.ElementAt(i).locY];
+            Instantiate(GM.Prefabs.Prefabs[stuffType], tileTospawn.transform.position, Quaternion.Euler(-90, 90, 0));
+            tileTospawn.impassible = true;
+            GM.BattleData.blockedTiles.Add(tileTospawn);
+        }
 	}
 
 	void saveMapToXml() {
