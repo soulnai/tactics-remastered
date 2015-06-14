7b5a9ac102204614dd2f804fe89d76ad4d558fb6
soulnai <soulnai@gmail.com>
2015-06-14 13:59:24 +0300

    preview generation changed

--- Assets/Scripts/ScrollableList.cs
+++ Assets/Scripts/ScrollableList.cs
@@ -61,11 +61,13 @@ public class ScrollableList : MonoBehaviour
             textFiled.text = Prefabs[i].name;
             Rect rec = new Rect(0, 0, 50, 50);
             Vector2 vec = new Vector2(0, 0);
-            /*
-             * Part of code to create prefab prewiew and save on disk
-             * 
+            
+             //Part of code to create prefab prewiew and save on disk
+              
             int counter = 0;
             Texture2D tex = null;
+            if (File.Exists(Application.dataPath + "/preview/imgs/" + Prefabs[i].name + ".png") == false)
+            {
             while (tex == null && counter < 20)
             {
                 tex = AssetPreview.GetAssetPreview(Prefabs[i]);
@@ -81,27 +83,28 @@ public class ScrollableList : MonoBehaviour
                 {
                     byte[] bytes = tex.EncodeToPNG();
                     File.WriteAllBytes(Application.dataPath + "/preview/imgs/" + Prefabs[i].name + ".png", bytes);
-                    textures[i] = tex;
+                    //textures[i] = tex;
                 }
             }
-            */
+            }
+            
 
             byte[] fileData;
-            int counter = 0;
-            //if (File.Exists(Application.dataPath + "/preview/imgs/" + Prefabs[i].name + ".png"))
-            //{
-                //fileData = null;
-                while (textures[i] == null && counter < 15)
+            counter = 0;
+            if (File.Exists(Application.dataPath + "/preview/imgs/" + Prefabs[i].name + ".png"))
+            {
+                fileData = null;
+                while (fileData == null && counter < 10)
                 {
-                    //fileData = File.ReadAllBytes(Application.dataPath + "/preview/imgs/" + Prefabs[i].name + ".png");
+                    fileData = File.ReadAllBytes(Application.dataPath + "/preview/imgs/" + Prefabs[i].name + ".png");
                     counter++;
-                    //textures[i] = new Texture2D(2, 2);            <-- part to load from disc and convert to sprite
-                    textures[i] = AssetPreview.GetAssetPreview(Prefabs[i]);
-                    //textures[i].LoadImage(fileData);            <-- part to load from disc and convert to sprite
-                    System.Threading.Thread.Sleep(150);
+                    textures[i] = new Texture2D(2, 2);            //<-- part to load from disc and convert to sprite
+                    //textures[i] = AssetPreview.GetAssetPreview(Prefabs[i]);
+                    textures[i].LoadImage(fileData);            //<-- part to load from disc and convert to sprite
+                    System.Threading.Thread.Sleep(50);
                 }
 
-                //textures[i].LoadImage(fileData);            <-- part to load from disc and convert to sprite
+                textures[i].LoadImage(fileData);           // <-- part to load from disc and convert to sprite
 
                 Sprite newsprite = new Sprite();
                 newsprite = Sprite.Create(textures[i], new Rect(0, 0, textures[i].width, textures[i].height), new Vector2(textures[i].width / 2, textures[i].height / 2),
@@ -113,7 +116,7 @@ public class ScrollableList : MonoBehaviour
                 //Add your new event
                 string name = Prefabs[i].name;
                 button.onClick.AddListener(() => MapCreatorManager.instance.handleButton(name));
-            //}
+            }
 
             
             newItem.transform.SetParent(gameObject.transform);
