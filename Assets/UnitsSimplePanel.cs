6945c447146a6439743e66126d37f7b95a6e34bf
andrew-chewie <vasiliev.art@gmail.com>
2015-07-07 00:31:56 +0300

    unit simple ui panel

---
+++ Assets/UnitsSimplePanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitsSimplePanel : MonoBehaviour {
+
+    public Player player;
+    public GameObject unitPanelPrefab;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    player = GM.BattleData.Players[0];
+        foreach (Unit unit in player.SpawnedPartyUnits)
+        {
+            GameObject go = Instantiate(unitPanelPrefab,Vector3.zero, Quaternion.identity) as GameObject;
+            go.transform.SetParent(transform,false);
+            go.GetComponent<UIWatchPanel>().UnitToWatch = unit;
+        }
+	}
+}
